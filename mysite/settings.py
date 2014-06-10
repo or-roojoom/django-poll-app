6c49a0b8748f50c0d79af8ad22fd8f3e4ede28a1
Or Meirov <or.meirov@gmail.com>
2014-06-10 19:07:45 +0300

    fix "evict" function

--- mysite/settings.py
+++ mysite/settings.py
@@ -16,7 +16,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'), # Or path to database file if using sqlite3.
+        'NAME': os.path.join(BASE_DIR, 'db2.sqlite3'), # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
@@ -129,7 +129,8 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
-    'polls'
+    'polls',
+    'explorer',
 )
 
 # A sample logging configuration. The only tangible logging
